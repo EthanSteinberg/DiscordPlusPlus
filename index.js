6fefce01f042e16812f0549f768e40b88852f0ef
Ethan Steinberg <ethan.steinberg@gmail.com>
2016-05-04 20:38:56 -0500

    CSS fix

--- index.js
+++
@@ -1,9 +0,0 @@
-// main.js
-
-var pageMod = require("sdk/page-mod");
-
-pageMod.PageMod({
-  include: "*.chat.stackoverflow.com",
-  contentScriptFile: ["./jquery.js", "./polyfill.js", "./background.js"],
-  contentStyleFile: "./style.css",
-});
